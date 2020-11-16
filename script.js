f204e8bd236c6c51437d76bc977e8530146552ff
Mskrost <74321599+Mskrost@users.noreply.github.com>
2020-11-16 21:37:31 +0100

    js update

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ let printNumbersTill = (bookCount) => {
     console.log(i)};
 };
 printNumbersTill(20);
+
+let getGreetingTo = (name) => {
+    return 'Hello ' + name;
+};
+console.log(getGreetingTo('Mark'));

e072336941ac7ad0df2547fc7359b4171251f3f0
Mskrost <74321599+Mskrost@users.noreply.github.com>
2020-11-16 20:15:08 +0100

    update

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 console.log('Janci');
 let printNumbersTill = (bookCount) => {
-    for (i = 0; i < bookCount; i++);
-    console.log(i);
+    for (i = 0; i < bookCount; i++) {
+    console.log(i)};
 };
 printNumbersTill(20);
